42b9cb78c0e753bf576f0b5b8efbe5fa96831a98
Dainik Bheda <107639433+dainik10@users.noreply.github.com>
2024-01-13 10:15:56 +0530

    Add files via upload

---
+++ DSA.py
@@ -0,0 +1,47 @@
+# DATA STRUCTURES:-
+# -----------------
+
+# --> Performing manupulation on data is called as data structures.
+# -- > Different manupulation on data are:-
+
+# 1) Storing the data 
+# 2) Updating the data 
+# 3) Retriving the data
+# 4) Deleting the data
+
+
+# --> Various data structure concept are:-
+
+# 1) Stack
+# 2) Queue
+# 3) Searching ans Sorting technique
+# 4) Linkedlist
+# 5) Trees
+# 6) Graph
+
+# # lINKED LIST:-
+#   --------------
+
+# -> It is a collection of nodes and everynode will be connected to the next node .
+
+# ==> It is clasified into three types.
+
+# 1) Single Linked list
+# 2) Double Linked list
+# 3) Circular Linked list
+
+
+# # Single Linkedlist:-
+# ---------------------
+
+# ---> it is a collection of nodes and everynode will contain two fields 
+# 1) data 
+# 2) next
+
+# ---> The various operations we are going to perform such as :-
+# 1) Creating the node
+# 2) Inserting the data into the node
+# 3) Displaying the data present in the node
+# 4) Adding the node
+# 5) Deleting the node
+# 6) Displaying the data in the reverse order.
